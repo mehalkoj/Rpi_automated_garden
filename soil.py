9f563b658b865ae58765a595b673b88f4e2f0851
mehalkoj <87507825+mehalkoj@users.noreply.github.com>
2022-01-18 22:36:40 -0600

    Add files via upload

---
+++ soil.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+ 
+#GPIO SETUP
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+ 
+def callback(channel):
+        if GPIO.input(channel):
+                print ("Water Detected!")
+        else:
+                print ("Water Detected!")
+ 
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=30)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+ 
+# infinite loop
+while True:
+        time.sleep(1)
+
+if __name__ == "__main__":
+    callback()
